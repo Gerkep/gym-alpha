4d9a64bcfd1357272327ea0f8f5b3fc1addc77d7
Gerkep <piotrg2003@gmail.com>
2022-04-03 14:12:00 +0200

    twitter update

--- metadataGenerator.js
+++ metadataGenerator.js
@@ -4,19 +4,19 @@ const fs = require('fs-extra');//importing file system extra module
 const metadataPath = path.resolve(__dirname, 'metadata');
 fs.ensureDirSync(metadataPath);
 
-for(let i = 1; i < 5; i++){
+for(let i = 101; i < 5778; i++){
     fs.outputJsonSync(
-     path.resolve(metadataPath, `data${i}.json`), 
+     path.resolve(metadataPath, `${i}.json`), 
      {
         "id": i,
         "name": `Gym Alpha Central #${i}`,
         "description": "Your pass to Gym Alpha headquoters.",
-        "image": "https://temp-cdn.coniun.io/coniun-pass-thumb.jpeg",
-        "animation url": `https://gymalpha.mypinata.cloud/ipfs/QmcT97VKky4VyBQimnvJ7LFyWgBYBbeUaXs6p6yNx213cj/main_blue${i}.mp4`,
+        "image": `https://gymalpha.mypinata.cloud/ipfs/Qmf1B38HZttHxEVPEL3L9Q9BG28RXPBBycZQb9fTZpLqwd/main_img_white${i-100}.jpg`,
+        "animation url": `https://gymalpha.mypinata.cloud/ipfs/QmcT97VKky4VyBQimnvJ7LFyWgBYBbeUaXs6p6yNx213cj/main_white${i-100}.mp4`,
         "attributes": [
             {
                 "trait_type": "Breed",
-                "value": "Mythic"
+                "value": "Common"
             }
         ]
     }
