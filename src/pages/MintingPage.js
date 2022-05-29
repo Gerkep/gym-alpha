351757fdf69cb22a6da82ff3cd6d7bdefaf06589
Gerkep <piotrg2003@gmail.com>
2022-05-29 13:55:16 +0200

    minting page added

---
+++ src/pages/MintingPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+class Minting extends React.Component{
+
+    render(){
+        return(
+            <div className="">
+                afgs
+            </div>
+        )
+    }
+
+}
+
+export default Minting;
