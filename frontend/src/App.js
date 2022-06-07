0f6561ebada016e52afef78b817d7183f479b16a
Gerkep <piotrg2003@gmail.com>
2022-06-07 14:13:54 +0200

    added main and store folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Router, Route, Switch} from 'react-router-dom';
-import DaoPage from "./pages/DaoPage";
-import MainPage from "./pages/MainPage";
-import StorePage from "./pages/StorePage";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import TokenPage from "./pages/TokenPage";
-import ErrorPage from "./pages/ErrorPage";
-import Minting from "./pages/MintingPage";
+import DaoPage from "./main/pages/DaoPage";
+import MainPage from "./main/pages/MainPage";
+import StorePage from "./main/pages/StorePage";
+import Navbar from "./main/components/Navbar";
+import Footer from "./main/components/Footer";
+import TokenPage from "./main/pages/TokenPage";
+import ErrorPage from "./main/pages/ErrorPage";
+import Minting from "./main/pages/MintingPage";
 import history from "./history";
 
-import "../src/style/main.css"
+import "../src/main/style/main.css"
 
 
  const App = () => {
